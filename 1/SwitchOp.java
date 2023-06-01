afe4fcdfc1add94d7ca00a1a0a8bcea544608778
jys4300 <w2322@e-mirim.hs.kr>
2023-06-01 23:33:00 +0900

    str

---
+++ 1/SwitchOp.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class SwitchOp
+{
+	public static void main(String args[])
+	{
+		int num1, num2;
+		String op;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("수식 입력(수와 연산자 사이는 공백을 둘 것) : ");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		op = sc.next();
+
+		switch(op)
+		{
+			case "+":
+				System.out.println(num1+ " + " +num2+ " = " +(num1+num2));	break;
+			case "-":
+				System.out.println(num1+ " - " +num2+ " = " +(num1-num2));	break;
+			case "*":
+				System.out.println(num1+ " * " +num2+ " = " +(num1*num2));	break;
+			case "/":
+				System.out.println(num1+ " / " +num2+ " = " +(num1/num2));	break;
+			
+			default:
+				System.out.println("사칙 연산자가 아닙니다.");
+			}
+	}
+}
---
+++ 1/str_1413.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class str
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		String str1, str2;
+		
+		System.out.println("문자를 입력하세요.");
+		str1 = sc.next();
+		System.out.println("문자를 입력하세요.");
+		str2 = sc.next();
+		System.out.println(st2 + " " + st1);
+	}
+}
