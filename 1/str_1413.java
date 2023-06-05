1718968dfb1d5eededca67e8e5ec3b8ad558df2a
주예설 <128341138+jys4300@users.noreply.github.com>
2023-06-05 23:56:27 +0900

    str_1413

---
+++ 1/str_1413.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Str
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		String str1, str2;
+		
+		System.out.print("문자를 입력하세요: ");
+		str1 = sc.next();
+		System.out.print("문자를 입력하세요: ");
+		str2 = sc.next();
+
+		System.out.println(str2 + " " + str1);
+	}
+} 
