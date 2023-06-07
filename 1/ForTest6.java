95aba6bbae77c90defbefe1f846835952153575a
주예설 <128341138+jys4300@users.noreply.github.com>
2023-06-08 03:30:03 +0900

    ForTest6

---
+++ 1/ForTest6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class ForTest6
+{
+	public static void main (String args[])
+	{
+		int i, num, result = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수 10개를 입력하세요 : ");
+	
+		for(i = 0; i < 10; i++){
+			num = sc.nextInt();
+		if(num % 3 == 0 || num % 5 == 0){
+			result = result + 1;
+			}	
+		}
+		System.out.print("3과 5의 배수의 개수 : " + result);	
+	}		
+}
