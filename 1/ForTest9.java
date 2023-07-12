271dcfdd256ed9fb8b616b85d79c6e3c35a04a06
주예설 <128341138+jys4300@users.noreply.github.com>
2023-07-12 23:09:00 +0900

    ForTest9

---
+++ 1/ForTest9.java
@@ -0,0 +1,12 @@
+class ForTest9
+{
+	public static void main(String[] args) 
+	{
+		for (int i=1; i<=5; i++) {
+			for (int a=1; a<=10; a++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
