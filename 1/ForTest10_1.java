c602d24352e463cdd8c4aba02c89890c0830623c
주예설 <128341138+jys4300@users.noreply.github.com>
2023-07-15 22:15:37 +0900

    ForTest10_1

---
+++ 1/ForTest10_1.java
@@ -0,0 +1,12 @@
+class ForTest10_1
+{
+	public static void main(String args[])
+	{
+		for (int i=1; i<=5; i++) {
+			for (int a=1; a<=5; a++) {
+				System.out.print(i+a);
+			}
+			System.out.println();
+		}
+	}
+}
